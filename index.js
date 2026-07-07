6717bfa1ec4292eec311bafbf95e98f1b34ba980
KingIVOfficial <arthurpsmith9@gmail.com>
2026-07-06 21:23:26 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.json({ ok: true });
+});
+
+app.listen(3000, () => {
+  console.log("Backend running on port 3000");
+});
