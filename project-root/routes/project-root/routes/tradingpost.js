5ec4bc9b387b6ac51e4c68e46cc8e0acb3c99ff2
KingIVOfficial <arthurpsmith9@gmail.com>
2026-07-07 16:55:13 -0400

    Delete project-root/routes/project-root/routes directory

--- project-root/routes/project-root/routes/tradingpost.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get('/health', (req, res) => {
-  res.json({ status: 'ok', service: 'Trading Post' });
-});
-
-export default router;
