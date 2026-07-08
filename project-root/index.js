81117826eaa5128355665dee55f2978130368d7b
KingIVOfficial <arthurpsmith9@gmail.com>
2026-07-08 16:40:00 -0400

    Update index.js

--- project-root/index.js
+++ project-root/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 
+// ROUTES
+const fallen = require('./routes/fallen.js');
+const tradingpost = require('./routes/tradingpost.js');
+
+// SERVICES
+const wallet = require('./services/sequenceWallet.js');
+
+// CONFIG
+const sequence = require('./config/sequence.js');
+
+// BASIC TEST ROUTE
 app.get('/', (req, res) => {
   res.send('OK');
 });
 
+// START SERVER (Railway compatible)
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
