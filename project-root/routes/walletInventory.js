4c15ea64cb3b64559c3b297184f4bd1dc5f1fe0d
KingIVOfficial <arthurpsmith9@gmail.com>
2026-07-08 20:10:51 -0400

    Update walletInventory.js

--- project-root/routes/walletInventory.js
+++ project-root/routes/walletInventory.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 /**
  * GET /wallet/inventory/:address
+ * Returns all items for a wallet across all networks.
  */
 router.get("/:address", async (req, res) => {
   const address = req.params.address;
